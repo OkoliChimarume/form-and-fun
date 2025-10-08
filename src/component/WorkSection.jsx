dc34607ad4347ba7b6f4699ff7007da9ccc48d0f
okoli Chimarume <juskie115@gmail.com>
2025-10-08 14:59:40 +0100

    chore: arrange work section grids

--- src/component/WorkSection.jsx
+++ src/component/WorkSection.jsx
@@ -12,7 +12,6 @@ export default function WorkSection() {
             <p className="text-gray-400 text-base">(22-25)</p>
           </div>
         </div>
-
         <section className="overflow-hidden whitespace-nowrap text-[#E5E5E5]">
           <div className="animate-slide flex">
             <p className="text-[6rem] font-[arial] tracking-tight px-8">
@@ -23,23 +22,22 @@ export default function WorkSection() {
             </p>
           </div>
         </section>
-
-        <div className="grid md:grid-cols-2 gap-8 mb-8">
-          <ProjectCard
-            title="Oreo x Pacman"
-            category="AR Experience"
-            aspect="landscape"
-            video="https://res.cloudinary.com/dm7vlpslq/video/upload/v1759642096/bheeicdi21w0hkxyqiby6vxovghp64qh2wan_qwmt7l.mp4"
-          />
+        <div className="grid gap-4 md:grid-cols-2 md:gap-x-4 lg:grid-cols-3 items-stretch p-3 md:p-4">
+          <div className="col-span-2 rounded-2xl">
+            <ProjectCard
+              title="Oreo x Pacman"
+              category="AR Experience"
+              aspect="landscape"
+              video="https://res.cloudinary.com/dm7vlpslq/video/upload/v1759642096/bheeicdi21w0hkxyqiby6vxovghp64qh2wan_qwmt7l.mp4"
+            />
+          </div>
           <ProjectCard
             title="Doordash Summer of Dashpass"
             category="Campaign"
             aspect="portrait"
             video="https://res.cloudinary.com/dm7vlpslq/video/upload/v1759642676/YThAIwoXKB_576p_1744824072_ccmg6f.mp4"
           />
-        </div>
 
-        <div className="grid md:grid-cols-2 gap-8">
           <ProjectCard
             title="Widllet"
             category="Brand Identity, App"
@@ -47,13 +45,16 @@ export default function WorkSection() {
             video="https://res.cloudinary.com/dm7vlpslq/video/upload/v1759642080/bheeicea8cwvkf21vcnbg8jhx3wo0wyb42im_q9yvuz.mp4"
             soon={true}
           />
-          <ProjectCard
-            title="Powerade Mindzone"
-            category="VR Experience"
-            aspect="landscape"
-            video="https://res.cloudinary.com/dm7vlpslq/video/upload/v1759642089/bheeicea3wowr2ifyqckn5737cnu42mewpr1_a8wqzo.mp4"
-          />
+          <div className="col-span-2 rounded-2xl">
+            <ProjectCard
+              title="Powerade Mindzone"
+              category="VR Experience"
+              aspect="landscape"
+              video="https://res.cloudinary.com/dm7vlpslq/video/upload/v1759642089/bheeicea3wowr2ifyqckn5737cnu42mewpr1_a8wqzo.mp4"
+            />
+          </div>
         </div>
+        ;
       </div>
     </section>
   );
